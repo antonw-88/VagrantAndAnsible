f2c1d5b23f6d8b1119d1dd09bef9c00d52dadd67
Atnon <atnon@localhost.localdomain>
2022-02-07 07:13:31 +0100

    update vagrant file

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,49 @@
-Vagrant.configure("2") do |config|
-  config.vm.define "master" do |subconfig|
-    subconfig.vm.box = "hashicorp/bionic64"
-    subconfig.vm.hostname = "master"
-    subconfig.vm.provider "virtualbox"
-    subconfig.vm.network "public_network", bridge: "BRIDGE"
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+# Practice boxes
+# Good commands to know:
+# vagrant status
+# vagrant destroy nameofbox
 
-    subconfig.vm.provision "ansible_local" do |ansible|
-      ansible.verbose = "v"
-      ansible.playbook = "provisioning/master_playbook.yaml"
-    end
+
+Vagrant.configure("2") do |config|
+
+  # Set std box to use, and configure their std stats
+  config.vm.box = "hashicorp/bionic64"
+  config.vm.provider :virtualbox do |v|
+    v.memory = 1024
+    v.cpus = 1
+    v.linked_clone = true #shared resources between boxes
   end
+
+  # //Various settings\\
+  # Use insecure key, since only testing environment
+  config.ssh.insert_key = "false"
+  # Turn off synced folder to isolate machines
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+
+  # Master ansible server box
+  config.vm.define "master" do |master|
+    master.vm.box = "hashicorp/bionic64"
+    master.vm.hostname = "master"
+    master.vm.network "private_network", ip: "192.168.56.2"
+  end
+
+  # AppBox1
+  config.vm.define "appbox1" do |appbox1|
+    appbox1.vm.hostname = "appbox1.test"
+    appbox1.vm.network :private_network, ip: "192.168.56.3"
+  end
+
+  # AppBox1
+  config.vm.define "appbox2" do |appbox2|
+    appbox2.vm.hostname = "appbox2.test"
+    appbox2.vm.network :private_network, ip: "192.168.56.4"
+  end
+
+
+
+
+
+
 end
--- install-script.sh
+++ install-script.sh
@@ -1,4 +1,4 @@
-# This is a script for RHEL(CentOS 8) - Kernel: 4.18.0-348.2.1.el8_5.x86_64
+# This is a script for RHEL(Rocky 8) - Kernel: 4.18.0-348.el8.0.2.x86_64
 # Install and configure Vagrant and Ansible so that they are ready for general usage
 # Preparation stages prior to running this script:
 # 0. Make sure that virtualization is enabled via BIOS
@@ -7,28 +7,31 @@
 
 # Installation of Vagrant
 # https://www.vagrantup.com/docs/index
-sudo yum install vagrant
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum install epel-release -y
+sudo yum install yum-utils -y
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo -y
 sudo yum install vagrant -y
-# Might need to install kernelmodule for virtualbox
-sudo dnf install akmod-VirtualBox kernel-devel-4.18.0-348.2.1.el8_5.x86_64
-sudo akmods --kernels 4.18.0-348.2.1.el8_5.x86_64 
-sudo systemctl restart vboxdrv.service
-#Initialize Vagrantfile
-vagrant init #hashicorp/bionic64
+
 
 # Installation of VirtualBox
-sudo yum install VirtualBox
+sudo dnf install gcc make perl kernel-devel kernel-headers bzip2 dkms -y
+sudo dnf update kernel-*
+sudo dnf config-manager --add-repo=https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
+sudo dnf update	
+sudo dnf install VirtualBox-6.1 -y
+# Might need to install kernelmodule for virtualbox
+sudo dnf install akmod-VirtualBox kernel-devel-4.18.0-348.2.1.el8_5.x86_64 -y
+sudo akmods --kernels 4.18.0-348.2.1.el8_5.x86_64 -y
+sudo systemctl restart vboxdrv.service
 # List of Vagrant boxes: https://app.vagrantup.com/boxes/search
 
 # Installation of Ansible
 # https://docs.ansible.com/
 # https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-rhel-centos-or-fedora
-sudo yum install epel-release
 sudo yum install ansible
 
-# Start the Vagrant box
+# Initialize Vagrant and start the box
+vagrant init hashicorp/bionic64
 vagrant up
 # Enter the box with $ vagrant ssh
 
